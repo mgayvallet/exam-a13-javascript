ea7884854261ec9f3767e0eccce8b0ba6bb9a822
qwyx <mc010116@gmail.com>
2025-03-27 09:31:32 +0100

    ex07.js

---
+++ ex07.js
@@ -0,0 +1,9 @@
+function afficherElements(tab) {
+    for (let i = 0; i < tab.length; i++) {
+        console.log(tab[i]);
+    }
+}
+
+
+afficherElements([1, 2, 3, 4]); // Affiche : 1, 2, 3, 4 (un par ligne)
+afficherElements([-1, 28, 44, 0]); // Affiche : -1, 28, 44, 0 (un par ligne)

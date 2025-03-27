5c754f64d0baec52731fbf18896238a06a3152a2
qwyx <mc010116@gmail.com>
2025-03-27 09:14:14 +0100

    feat(app) finish 03

---
+++ ex03.js
@@ -0,0 +1,9 @@
+function sommeTroisNombres(a, b, c) {
+    return a + b + c
+}
+
+console.log(sommeTroisNombres(5, 10, 15)); // Résultat attendu : 30
+console.log(sommeTroisNombres(0, -1, 2)); // Résultat attendu : 1
+console.log(sommeTroisNombres(0, -1, 20)); // Résultat attendu : 19
+console.log(sommeTroisNombres(200, -100, -100)); // Résultat attendu : 0
+console.log(sommeTroisNombres(44, 10, 6)); // Résultat attendu : 60

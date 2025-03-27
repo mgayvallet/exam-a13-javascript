dea5cc79c4fb9db2ea95d8becec319a9e1df6d14
qwyx <mc010116@gmail.com>
2025-03-27 09:11:03 +0100

    feat(app) finish 01

---
+++ ex01.js
@@ -0,0 +1,8 @@
+function multiplication(a, b) {
+    return a * b
+}
+
+console.log(multiplication(4, 5)); // Résultat attendu : 20
+console.log(multiplication(-1, 15)); // Résultat attendu : -15
+console.log(multiplication(2, 5)); // Résultat attendu : 10
+console.log(multiplication(9, 9)); // Résultat attendu : 81

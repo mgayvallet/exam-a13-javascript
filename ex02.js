04021553a320635e971ebfa718ea53909ab2b411
qwyx <mc010116@gmail.com>
2025-03-27 09:12:30 +0100

    feat(app) finish 03

---
+++ ex02.js
@@ -0,0 +1,9 @@
+function carre(nombre) {
+    return nombre * nombre
+}
+
+console.log(carre(4)); // Résultat attendu : 16
+console.log(carre(2)); // Résultat attendu : 4
+console.log(carre(7)); // Résultat attendu : 49
+console.log(carre(9)); // Résultat attendu : 81
+console.log(carre(10)); // Résultat attendu : 100
